536db7b9a8e62d6e5cdf884c1d2b84ca2d54563b
Ari <ari.weitzman@gmail.com>
2015-07-14 16:49:30 -0400

    Create route from static view to create form for new quest.

--- config/routes.rb
+++ config/routes.rb
@@ -4,35 +4,9 @@ Rails.application.routes.draw do
   resources :users,  only: [:new, :create, :show, :edit, :index, :destroy]
   resource :session, only: [:new, :create, :destroy]
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  namespace :api, defaults: { format: :json } do
+    resources :quests, only: [:new, :create, :show, :edit, :destroy]
+  end
 
   # Example resource route with more complex sub-resources:
   #   resources :products do
