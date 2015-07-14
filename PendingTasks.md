a5d95d3a9287932eabaa352b5245fe15c1ae3e30
App Academy Student <appacademy@Apps-Mac-mini-6.local>
2015-07-14 11:09:23 -0400

    Create seeds.

--- PendingTasks.md
+++ PendingTasks.md
@@ -1,2 +1,3 @@
 * Add a username feature to a users table that is unique within a group, but not
 necessarily unique across the users table.
+* Default leader_id to user.id if not invited
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(
+  email: "finn@ooo.com",
+  is_leader: true,
+  leader_id: 1,
+  password: "bubblegum",
+)
+
+User.create!(
+  email: "jake@ooo.com",
+  is_leader: false,
+  leader_id: 1,
+  password: "bubblegum",
+)
+
+User.create!(
+  email: "bmo@ooo.com",
+  is_leader: false,
+  leader_id: 1,
+  password: "bubblegum",
+)
