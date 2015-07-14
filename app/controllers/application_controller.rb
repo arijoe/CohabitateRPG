f32e895fc84af97d6732c62243edf63225e2b8cd
App Academy Student <appacademy@Apps-Mac-mini-6.local>
2015-07-14 11:47:59 -0400

    Create application controller with logged-in/current user methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,32 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?, :logout!, :login!
+
+  private
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token
+  end
+
+  def logout!
+    session[:session_token] = nil
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def require_logged_in!
+    redirect_to new_session_url if !logged_in?
+  end
+
+  def require_logged_out!
+    redirect_to user_url(current_user) if logged_in?
+  end
 end
