c44477acb4e351037edb5902ca74e3fce79ce343
Ari <ari.weitzman@gmail.com>
2015-07-15 14:04:33 -0400

    Fix instantiation of router.

--- app/assets/javascripts/cohabitate.js
+++ app/assets/javascripts/cohabitate.js
@@ -5,8 +5,8 @@ window.Cohabitate = {
   Routers: {},
   initialize: function() {
     new Cohabitate.Routers.Router({
-      $rootEl = #("main")
-      collection: new Cohabitate.Collections.quests();
+      $rootEl: ("#main"),
+      collection: new Cohabitate.Collections.quests
     });
     Backbone.history.start();
   }
