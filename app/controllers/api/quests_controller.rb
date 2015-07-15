9e15e967112f7a5b0a96cbff66c19ebbf8a609dd
Ari <ari.weitzman@gmail.com>
2015-07-15 11:37:38 -0400

    Create controller functions for api::quest.

--- app/controllers/api/quests_controller.rb
+++ app/controllers/api/quests_controller.rb
@@ -1,3 +1,42 @@
   class Api::QuestsController < ApplicationController
+    def create
+      @quest = current_user.quests.new(quest_params)
 
+      if @quest.save
+        render json: @quest
+      else
+        render json: @quest.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    def show
+      @quest = Quest.find(params[:id])
+
+      if @quest
+        render :show
+      else
+        render json: ["None such quest, compadre."], status: 404
+      end
+    end
+
+    def edit
+      @quest = Quest.find(params[:id])
+
+      if @quest.save
+        render json: @quest
+      else
+        render json: @quest.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @quest = current_user.quests.find(params[:id])
+      @quest.destroy
+      render json: {}
+    end
+
+    private
+    def quest_params
+      params.require(:quest).permit(:title)
+    end
   end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,17 +1,3 @@
 class StaticController < ApplicationController
-  def create
-
-  end
-
-  def show
-
-  end
-
-  def edit
-
-  end
-
-  def destroy
-
-  end
+  
 end
