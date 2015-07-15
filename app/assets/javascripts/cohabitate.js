ccbb9923ea6867a8f0fffef3511b37341f447e54
Ari <ari.weitzman@gmail.com>
2015-07-15 13:56:58 -0400

    Add backbone-on-rails and generate files.

---
+++ app/assets/javascripts/cohabitate.js
@@ -0,0 +1,13 @@
+window.Cohabitate = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Cohabitate.initialize();
+});
