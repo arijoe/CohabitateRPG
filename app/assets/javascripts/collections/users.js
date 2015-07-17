9e18fdff500955a0f9c7e15133045e756c5563aa
App Academy Student <appacademy@Apps-Mac-mini-29.local>
2015-07-17 11:00:10 -0400

    Add users.js collection to assets.

---
+++ app/assets/javascripts/collections/users.js
@@ -0,0 +1,22 @@
+Cohabitate.Collections.Users = Backbone.Collection.extend({
+  url: "/api/users",
+  model: Cohabitate.Models.User,
+
+  getOrFetch: function(id) {
+    var user = this.get(id),
+        users = this;
+
+    if(!user) {
+      user = new this.model({ id: id });
+      user.fetch({
+        success: function() {
+          users.add(user);
+        }
+      });
+    } else {
+      user.fetch();
+    }
+
+    return user;
+  }
+});
