c48c555e03ad690d2de8675f3b5a64441d56be49
arijoe <ari.weitzman@gmail.com>
2015-07-22 09:30:53 -0400

    Start composite view for quest-bar show.

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -45,7 +45,7 @@ Cohabitate.Routers.Router = Backbone.Router.extend ({
 
   },
 
-  _swapView: function (view, el) {
+  _swapView: function (view) {
     this._currentView && this._currentView.remove();
     this._currentView = view;
     this.$rootEl.html(view.render().$el);
--- app/assets/javascripts/views/gamebar/show.js
+++ app/assets/javascripts/views/gamebar/show.js
@@ -1,13 +1,22 @@
 Cohabitate.Views.GamebarShow = Backbone.CompositeView.extend({
-  // template: JST['quests/bar_show'],
-  //
-  // events: {
-  //
-  // },
-  //
-  // render: function () {
-  //   var content = this.template({ lists: this.collection });
-  //   this.$el.html(content);
-  //   return this;
-  // }
+  template: JST['quests/bar_show'],
+
+  initialize: function (options) {
+    this.user = options.user,
+    this.quest = options.quest
+  },
+
+  events: {
+
+  },
+
+  renderProfileDisplay: function () {
+    
+  },
+
+  render: function () {
+    var content = this.template({ quest: this.quest });
+    this.$el.html(content);
+    return this;
+  }
 });
